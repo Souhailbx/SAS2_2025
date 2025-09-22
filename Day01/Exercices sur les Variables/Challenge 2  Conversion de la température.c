1d020da673f57d2fa1c42f1eaae105d03903985d
Souhailbx <87235470+Souhailbx@users.noreply.github.com>
2025-09-22 16:55:09 +0100

    Update Challenge 2  Conversion de la température.c

--- Day01/Exercices sur les Variables/Challenge 2  Conversion de la température.c
+++ Day01/Exercices sur les Variables/Challenge 2  Conversion de la température.c
@@ -4,7 +4,7 @@ int main() {
     float T ;
     float K;
     printf("Vuillez Entrez la temperature qui vou voullez changer :");
-    scanf("%d",&T);
+    scanf("%f",&T);
     K = (T + 273.15);
     printf("La temperature en Kelvin est :%.2f",K);
 return 0;
