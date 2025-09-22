1871b57fd25e6208200bee9c733685423b946dcd
SOUHAIL TOURABI <souhailtourabi90@gmail.com>
2025-09-22 16:53:20 +0100

    ADD CODE de Challenge 3  Conversion de la distance.c

---
+++ Day01/Exercices sur les Variables/Challenge 3  Conversion de la distance.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float KM ;
+    float YA;
+    printf("Vuillez Entrez la Distance qui vou voullez transforme en KM:");
+    scanf("%f",&KM);
+    YA = (KM * 1093.61);
+    printf("La temperature en Kelvin est :%.2f Yards",YA);
+return 0;
+}
