8f4d2bbe4cf6d9708f2dca3b0fea7333de9a3f16
SOUHAIL TOURABI <souhailtourabi90@gmail.com>
2025-09-24 12:23:08 +0100

    Challenge 6  Multiplication des Éléments

---
+++ Day02/Exercices sur les Tableaux en C/Challenge 6  Multiplication des Éléments.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main() {
+    int n, i, facteur;
+
+    printf("Entrez le nombre d'elements du tableau: ");
+    scanf("%d", &n);
+
+    int tableau[n];
+    printf("Entrez les elements du tableau:\n");
+    for(i = 0; i < n; i++) {
+        printf("Element %d: ", i+1);
+        scanf("%d", &tableau[i]);
+    }
+    printf("Entrez le facteur de multiplication: ");
+    scanf("%d", &facteur);
+
+    printf("Tableau apres multiplication:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d x %d = %d\n", tableau[i],facteur,tableau[i]*facteur);
+    }
+
+    return 0;
+}
