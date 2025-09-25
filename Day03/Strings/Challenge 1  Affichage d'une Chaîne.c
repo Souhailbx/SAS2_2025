8e2b078b48eb317c1b2efcecb1dea1eb1b3634c4
SOUHAIL TOURABI <souhailtourabi90@gmail.com>
2025-09-25 15:28:12 +0100

    Challenge 1  Affichage d'une Chaîne

---
+++ Day03/Strings/Challenge 1  Affichage d'une Chaîne.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char CH[10];
+    printf("Veuillez entrez un chaine de caracter :");
+    scanf("%s",&CH);
+    printf("tu es entrer :%s",CH);
+    return 0;
+}
