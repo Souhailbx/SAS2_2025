dbefbe58881eb7d38dfefeb460a10805490b4014
SOUHAIL TOURABI <souhailtourabi90@gmail.com>
2025-09-26 22:06:25 +0100

    Challenge12_NombreEntierInverse.c

--- Day01/Exercices sur les Variables/Challenge12_NombreEntierInverse.c
+++ Day01/Exercices sur les Variables/Challenge12_NombreEntierInverse.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <math.h>
 int main() {
-    char n[5];
-    printf("Entrez un entier a 4 chiffres : ");
-    scanf("%4s", n);
-
-    printf("L'inverse est : %c%c%c%c\n", n[3], n[2], n[1], n[0]);
+      int N ,A ,B ,C ,D;
+      printf("Entrez le nombre:");
+      scanf("%d",&N);
+        A = N%10;
+        B = (N/10)%10;
+        C = (N/100)%10;
+        D = (N/1000)%10;
+      printf("  Le nombre a l'inverse est :%d%d%d%d",A,B,C,D);
 
     return 0;
+
+
 }
