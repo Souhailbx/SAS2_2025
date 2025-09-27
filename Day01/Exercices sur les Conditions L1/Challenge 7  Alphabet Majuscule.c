4f0d3d90c34d92b2c413c831bacdb0bc5e6a42da
SOUHAIL TOURABI <souhailtourabi90@gmail.com>
2025-09-27 21:34:17 +0100

    Challenge 7  Alphabet Majuscule

---
+++ Day01/Exercices sur les Conditions L1/Challenge 7  Alphabet Majuscule.c
@@ -0,0 +1,16 @@
+#include stdio.h
+#include stdlib.h
+#include math.h
+int main() {
+    char C;
+    printf(Entrez une caractere );
+    scanf(%c,&C);
+    if (C =65 &&C=90){
+        printf(Cette cractere est majuscule );
+    }else{
+    printf(cette nombre n'est pas majuscule);
+    }
+    return 0;
+
+
+}
