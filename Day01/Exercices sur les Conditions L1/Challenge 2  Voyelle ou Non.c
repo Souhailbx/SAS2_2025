12365d783d4caeadc8c2ab8ec41cb224382593cb
SOUHAIL TOURABI <souhailtourabi90@gmail.com>
2025-09-27 13:28:13 +0100

    Challenge 2  Voyelle ou Non.c

---
+++ Day01/Exercices sur les Conditions L1/Challenge 2  Voyelle ou Non.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main() {
+           char C;
+            printf("Entrez une char :");
+            scanf("%c",&C);
+            switch (C){
+                case 'A':printf("c'est une voyelle");
+                break;
+                case 'E':printf("c'est une voyelle");
+                break;
+                case 'I':printf("c'est une voyelle");
+                break;
+                case 'O':printf("c'est une voyelle");
+                break;
+                case 'U':printf("c'est une voyelle");
+                break;
+                case 'Y':printf("c'est une voyelle");
+                break;
+
+            default: printf("C'est pas une voyelle");
+
+            }
+
+
+    return 0;
+
+
+}
